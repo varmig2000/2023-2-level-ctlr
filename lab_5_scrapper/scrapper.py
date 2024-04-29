4384f1402bb6272a6a4def588be1d6d20f48a373
Varvara <varmig2000@gmail.com>
2024-04-29 13:50:21 +0300

    step 3.3

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -6,14 +6,16 @@ import json
 # pylint: disable=too-many-arguments, too-many-instance-attributes, unused-import, undefined-variable
 import pathlib
 import re
+import shutil
 from typing import Pattern, Union
+
 import requests
 from bs4 import BeautifulSoup
+
 from core_utils.article.article import Article
 from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
 from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
-import shutil
 
 
 class IncorrectSeedURLError(Exception):
